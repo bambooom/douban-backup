06a99ecec2b6d12218953716dcea0c9e097b1084
zhuzi <nmz89825@gmail.com>
2021-06-09 00:18:01 +0800

    :bookmark: change: update export userscript name.

--- export.user.js
+++ export.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         豆瓣读书+电影+音乐+游戏导出工具
+// @name         豆瓣读书+电影+音乐+游戏+舞台剧导出工具
 // @namespace    https://www.douban.com/people/MoNoMilky/
-// @version      0.3
+// @version      0.3.1
 // @description  扩展修改自 https://openuserjs.org/scripts/KiseXu/%E8%B1%86%E7%93%A3%E7%94%B5%E5%BD%B1%E5%AF%BC%E5%87%BA%E5%B7%A5%E5%85%B7
 // @author       Bambooom
 // @match        https://book.douban.com/people/*/collect*
