47e597a1b503dc6e12d13cb73570a45cad24eca8
zhuzi <nmz89825@gmail.com>
2021-07-09 00:01:08 +0800

    :hankey: wip: export note userscript.

---
+++ export-note.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name             豆瓣日记导出工具
+// @namespace        https://www.douban.com/people/MoNoMilky/
+// @version          0.1
+// @description      将豆瓣日记导出为 markdown
+// @author           Bambooom
+// @icon             https://www.google.com/s2/favicons?domain=douban.com
+// @match            https://www.douban.com/people/*/notes*
+// @require          https://unpkg.com/dexie@latest/dist/dexie.js
+// @require          https://unpkg.com/turndown/dist/turndown.js
+// @grant            none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var tdService = new TurndownService();
+
+  function escapeQuote(str) {
+    return str.replaceAll('"', '""'); // " need to be replaced with two quotes to escape inside csv quoted string
+  }
+
+  // @todo: use button to activate, and also save to dexie and export all md files
+  //$('#content .aside .create-note a:last').after('&nbsp; &nbsp;<a href="?start=0&type=note&export=1" onclick="">导出日记</a>');
+
+  var notes = $('.note-container[id^="note-"]');
+  notes.each(function (index) {
+    var note = $(this);
+    var title = escapeQuote(note.find('.note-header-container a').text().trim());
+    console.log(title);
+    var toggle = note.find('.note-header-container .rr a:first'); // 展开全文
+    toggle.click();
+
+    setTimeout(function() {
+      let dom = note.find('[id^="note_"][id$="_full"] .note')[0];
+      let md = tdService.turndown(dom);
+      // @todo: add rule to turndown, more specify for douban note style
+
+      console.log(md);
+    }, 1500);
+  });
+
+
+})();
