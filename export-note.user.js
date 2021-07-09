b43eb54bdd92e7f3f58c80a82e0749cb83a21f3c
zhuzi <nmz89825@gmail.com>
2021-07-10 00:58:23 +0800

    :hankey: feature: add rule for highlight block.

--- export-note.user.js
+++ export-note.user.js
@@ -14,7 +14,25 @@
 (function () {
   'use strict';
 
-  var tdService = new TurndownService();
+  var tdService = new TurndownService({
+    headingStyle: 'atx', // use # for header
+  });
+
+  // highlight-block => ref block `>`
+  tdService.addRule('highlight-block', {
+    filter: function(node) {
+      return (
+        node.nodeName === 'DIV' &&
+        node.className === 'highlight-block'
+      );
+    },
+    replacement: function (content) {
+      return '> ' + content;
+    },
+  });
+
+  // @todo: span font-weight: bold => **bold**
+  // @todo: keep <mark>
 
   function escapeQuote(str) {
     return str.replaceAll('"', '""'); // " need to be replaced with two quotes to escape inside csv quoted string
@@ -29,15 +47,26 @@
     var title = escapeQuote(note.find('.note-header-container a').text().trim());
     console.log(title);
     var toggle = note.find('.note-header-container .rr a:first'); // 展开全文
-    toggle.click();
+    // toggle.click();
 
-    setTimeout(function() {
-      let dom = note.find('[id^="note_"][id$="_full"] .note')[0];
-      let md = tdService.turndown(dom);
-      // @todo: add rule to turndown, more specify for douban note style
+    // @fixme: test for only one note is enough
+    if (index === 0) {
+      toggle.click();
 
-      console.log(md);
-    }, 1500);
+      setTimeout(() => {
+        let dom = note.find('[id^="note_"][id$="_full"] .note')[0];
+        let md = tdService.turndown(dom);
+        console.log(md);
+      }, 1500);
+    }
+
+    // setTimeout(function() {
+    //   let dom = note.find('[id^="note_"][id$="_full"] .note')[0];
+    //   let md = tdService.turndown(dom);
+    //   // @todo: add rule to turndown, more specify for douban note style
+
+    //   // console.log(md);
+    // }, 1500);
   });
 
 
